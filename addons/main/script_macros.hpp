6b5aba923a561978b533f834c09f4c00b7db1371
Sven Steinbach <ste-sven@gmx.de>
2022-11-28 21:40:51 +0100

    Fix:    - fixed gettting of painSuppress of ace medical

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -7,6 +7,6 @@
 #define QACEVAR(comp,var) #ACEVAR(comp,var)
 
 #define ACE_GET_PAIN(unit) (unit getVariable [QACEVAR(medical,pain), 0])
-#define ACE_GET_PAIN_SUPP(unit) (unit getVariable [QACEVAR(medical,pain_suppress),0])
+#define ACE_GET_PAIN_SUPP(unit) (unit getVariable [QACEVAR(medical,painSuppress),0])
 #define ACE_GET_PAIN_PERCEIVED(unit) (0 max (ACE_GET_PAIN(unit) - ACE_GET_PAIN_SUPP(unit)) min 1)
 
